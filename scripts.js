670736ed18a0d460af7b92aabe96ca0f233d3315
Pat Kelly iMedia <pkelly@imediainc.com>
2022-05-11 16:10:01 -0400

    Adding in JS/html to update copyright date

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let mobileToggleIcon = document.querySelector("#mobile-toggle-icon");
 let navLinks         = document.querySelectorAll(".nav-link");
 let navbarCollapse   = document.querySelector(".navbar-collapse");
+let copyrightDate    = document.querySelector("#copyright-date");
 // Looping through all navLinks and adding an event listener to close navbar & change icon when clicked (made for mobile) //
 navLinks.forEach(link => {
     link.addEventListener("click", () => {
@@ -16,3 +17,5 @@ mobileToggleIcon.addEventListener("click", () => {
         mobileToggleIcon.innerHTML = '<i class="fas fa-bars"></i>'
     }
 });
+// Update Copyright Date with current year //
+copyrightDate.innerText = new Date().getFullYear();
